441fe1ff7c7795addc27449b9bb1f5de07c9cf07
币阁~舞动的代码 <gana1007@163.com>
2018-03-17 11:05:21 +0800

    误传

--- springboot-demo1/src/main/java/com/example/demo/config/v3/WebSocketConfigV3.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.config.v3;
-
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-//EnableWebSocketMessageBroker已在Spring 4中引入。EnableWebSocketMessageBroker注释使配置类能够支持WebSocket。此注释支持消息代理
-@EnableWebSocketMessageBroker() 
-public class WebSocketConfigV3 {
-
-}
